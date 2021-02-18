7ac0a066115cdd1c39ef5ce91e10f1f204c0d904
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2021-02-18 16:56:39 +0300

    2nd day is over

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ $('.top__slider').slick({
     autoplay: true,
     fade: true
 });
+
+$('.reviews__slider').slick({
+    arrows:false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+});
 });
