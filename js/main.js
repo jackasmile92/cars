77c69016c97b1267ac43c2a843d0adc7dee2e6fd
Ekaterina Antonova <antonova.ekaterina@accenture.com>
2021-03-09 16:13:39 +0300

    continuing adaptation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ $('.top__slider').slick({
     arrows:false,
     dots: true,
     autoplay: true,
-    fade: true
+    fade: true,
+    responsive:[{
+        breakpoint: 1200,
+        settings:{
+            dots:false
+        }
+    }]
 });
 
 $('.reviews__slider').slick({
@@ -12,4 +18,9 @@ $('.reviews__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1
 });
+
+$('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+});
+
 });
